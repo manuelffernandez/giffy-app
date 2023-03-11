712365bb2d6f51e54a37a0cc2ebada0b008742c3
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-03-11 01:01:01 -0300

    adds comp to barrel file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export * from '@/components/Footer';
 export * from '@/components/Header';
+export * from '@/components/GifItem';
+export * from '@/components/GifList';
+export * from '@/components/SearchBar';
