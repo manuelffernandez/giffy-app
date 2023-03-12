77a64670cd61efacac7609e46a521f4bfa2b1eaa
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-03-11 21:41:37 -0300

    creates search page

---
+++ src/pages/Search/Search.tsx
@@ -0,0 +1,21 @@
+import { GifList, SearchBar } from '@/components';
+import { useSearchBar } from '@/hooks';
+import { useLocation, useParams } from 'react-router-dom';
+
+const Search = (): JSX.Element => {
+  const { queryTerm } = useParams();
+  const location = useLocation();
+  const { gifs } = location.state;
+  const { handleSearch, isLoading } = useSearchBar();
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <>
+      <SearchBar onSearch={handleSearch} queryTerm={queryTerm as string} />
+      <GifList gifs={gifs} queryTerm={queryTerm as string} />
+    </>
+  );
+};
+
+export default Search;
