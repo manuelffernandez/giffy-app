14eaf5158c744efa91c489179a7cafd287883ea3
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-03-15 16:20:26 -0300

    GifItem links to DetailPage & minor changes in GifList

--- src/components/GifItem/GifItem.tsx
+++ src/components/GifItem/GifItem.tsx
@@ -1,32 +1,36 @@
+import { CustomRouterLink } from '@/styledComponents';
 import { Box, ImageListItem, Typography } from '@mui/material';
 
 interface Props {
   title: string;
   url: string;
+  id: string;
 }
 
 const GifItem = (props: Props): JSX.Element => {
-  const { title, url } = props;
+  const { title, url, id } = props;
   return (
     <ImageListItem
       sx={{
         lineHeight: 0,
         position: 'relative',
       }}>
-      <Box component='img' src={url} loading='lazy' width='100%'></Box>
-      <Typography
-        sx={{
-          fontSize: 'small',
-          paddingLeft: 1,
-          paddingRight: 1,
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          bgcolor: 'rgba(0, 0, 0, 0.6)',
-          color: 'white',
-        }}>
-        {title}
-      </Typography>
+      <CustomRouterLink to={`/gif/${id}`}>
+        <Box component='img' src={url} loading='lazy' width='100%'></Box>
+        <Typography
+          sx={{
+            fontSize: 'small',
+            paddingLeft: 1,
+            paddingRight: 1,
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            bgcolor: 'rgba(0, 0, 0, 0.6)',
+            color: 'white',
+          }}>
+          {title}
+        </Typography>
+      </CustomRouterLink>
     </ImageListItem>
   );
 };
