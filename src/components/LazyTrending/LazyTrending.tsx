6480ae704acfa9147a3797e49cc2b709b4d5d826
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-03-17 14:27:52 -0300

    integrates Suspense and lazy react into LazyTrending

--- src/components/LazyTrending/LazyTrending.tsx
+++ src/components/LazyTrending/LazyTrending.tsx
@@ -1,11 +1,21 @@
 import { useLazyLoad } from '@/hooks';
 import { Box } from '@mui/material';
-import { TrendingSearches } from './components';
+import { lazy, Suspense } from 'react';
+
+const TrendingSearches = lazy(
+  async () => await import('./components/TrendingSearches')
+);
 
 const LazyTrending = (): JSX.Element => {
   const { isNear, fromRef } = useLazyLoad();
 
-  return <Box ref={fromRef}>{isNear ? <TrendingSearches /> : null}</Box>;
+  return (
+    <Box ref={fromRef}>
+      <Suspense fallback={'cargando...'}>
+        {isNear ? <TrendingSearches /> : null}
+      </Suspense>
+    </Box>
+  );
 };
 
 export default LazyTrending;
