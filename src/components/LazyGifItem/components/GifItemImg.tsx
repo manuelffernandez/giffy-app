18b00bf89b0e7959c4dea3059e2ff5f05a8c7f93
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-03-21 13:51:29 -0300

    add alt attribute to GifItemImg

--- src/components/LazyGifItem/components/GifItemImg.tsx
+++ src/components/LazyGifItem/components/GifItemImg.tsx
@@ -2,11 +2,12 @@ import { type ReactJSXElement } from '@emotion/react/types/jsx-namespace';
 
 interface Props {
   src: string;
+  alt: string;
 }
 
 const GifItemImg = (props: Props): ReactJSXElement => {
-  const { src } = props;
-  return <img width='100%' height='100%' src={src} />;
+  const { src, alt } = props;
+  return <img width='100%' height='100%' src={src} alt={alt} />;
 };
 
 export default GifItemImg;
