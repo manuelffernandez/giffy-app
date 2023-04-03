62cd37402378116be28116b07cd4c11f5b0ff982
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-03 18:37:18 -0300

    add toggle color mode button in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,28 @@
+import { useColorMode } from '@/hooks/useColorMode';
 import { CustomRouterLink } from '@/styledComponents/CustomRouterLink';
 import GifIcon from '@mui/icons-material/Gif';
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, IconButton, Toolbar } from '@mui/material';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 const Header = (): JSX.Element => {
+  const { colorMode, actualMode } = useColorMode();
+
+  const handleClick = (): void => {
+    colorMode.toggleColorMode();
+  };
+
   return (
     <AppBar position='sticky' sx={{ mb: 5, backgroundColor: 'primary.main' }}>
-      <Toolbar>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <CustomRouterLink to='/'>
           <GifIcon fontSize='large' />
         </CustomRouterLink>
+        <IconButton
+          onClick={handleClick}
+          sx={{ color: 'primary.contrastText' }}>
+          {actualMode === 'light' ? <LightModeIcon /> : <DarkModeIcon />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
